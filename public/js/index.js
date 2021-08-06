2eb760e9da01bb655284a4e5bc9f76fd1ffbd6f4
Douglas Masho <douglasmasho@gmail.com>
2021-08-07 00:05:28 +0200

    finished the homepage

---
+++ public/js/index.js
@@ -0,0 +1,33 @@
+AOS.init();
+const menuToggle = document.querySelector(".toggle");
+const showcase = document.querySelector(".showcase");
+const overlay = document.querySelector(".overlay");
+const therest = document.querySelector(".therest");
+
+
+
+menuToggle.addEventListener("click", ()=>{
+    menuToggle.classList.toggle("active");
+    showcase.classList.toggle("active");
+    overlay.classList.toggle("active");
+    therest.classList.toggle("active");
+})
+
+
+function scrollanimation(element, classname){
+    console.log(document.querySelector(`.${element}`));
+    $(window).on('scroll', function(){
+        if ($(window).scrollTop()){
+            // $(element).addClass(classname);
+            document.querySelector(`.${element}`).classList.add(classname);
+        }
+        else 
+        {
+            $(element).removeClass(classname); 
+            document.querySelector(`.${element}`).classList.remove(classname);
+
+        }
+    })
+};
+scrollanimation("svglogo", "another");
+scrollanimation("header", "black");
